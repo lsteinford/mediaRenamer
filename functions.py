88d00187db719aacb152c5940c2848183d1bfb76
Luke Steinford <lsteinford@nic.edu>
2024-12-01 20:41:56 -0800

    Fix sub-title delimiter

--- functions.py
+++ functions.py
@@ -4,36 +4,81 @@ import requests
 import json
 import re
 from gui import *
+from dotenv import load_dotenv 
 
-apiKey = "4bfc1c97"
+load_dotenv()
+apiKey = os.getenv("API_KEY")
 
-def get_file_extension(file_name):
-    _, extension = os.path.splitext(file_name)
-    return extension
+# Movie Functions
 
-def update_example(name_combo, info_combo, sub_combo, example_text, notebook, episode_title, lowercase):
-    selected_tab = notebook.tab(notebook.select(), "text")
-
-    media_type = 1 if selected_tab == "Series" else 0
-    
-    name_delim = name_combo.get()
-    info_delim = info_combo.get()
-    sub_delim = sub_combo.get()
-    example_names = ["Media","Name","Episode","Sub"]
-
-    if lowercase.get() == 1:
-        example_names = [name.lower() for name in example_names]
-            
-
-    if media_type == 0:
-        example = f"{example_names[0]}{name_delim}{example_names[1]}{sub_delim}{example_names[3]}{name_delim}{example_names[1]}{info_delim}YEAR.ext"
+# To do: else should search imdb for the year of the movie and pull it if needed
+#        however, this will likely never be an issue since most files usually come
+#        with the year in the file name
+def extract_year(file_name):
+    pattern = r"(\d{4})"
+    match = re.search(pattern, file_name, re.IGNORECASE)
+    if match:
+        return match.group(1)
     else:
-        if episode_title.get() == 1:
-            example = f"{example_names[0]}{name_delim}{example_names[1]}{sub_delim}{example_names[3]}{name_delim}{example_names[1]}{info_delim}S00E00{info_delim}{example_names[2]}{name_delim}{example_names[1]}.ext"
-        else:
-            example = f"{example_names[0]}{name_delim}{example_names[1]}{sub_delim}{example_names[3]}{name_delim}{example_names[1]}{info_delim}S00E00.ext"
-    example_text.delete("1.0", tk.END) 
-    example_text.insert(tk.END, example)
+        print(f"Error extracting year from {file_name}")
+        return None
+
+def name_movie(file_name, name_combo, info_combo, sub_combo, lowercase):
+    info_delim = info_combo.get()
+    name_delim = name_combo.get()
+    sub_delim = sub_combo.get()
+    show_name = detect_name(file_name, name_delim, sub_delim)
+    year = extract_year(file_name)
+    file_ext = get_file_extension(file_name)
+    file_name = f"{show_name}{info_delim}{year}{file_ext}"
+    if lowercase.get() == 1:
+        return file_name.lower()
+    else:
+        return file_name
+
+# Series Functions
+
+def extract_season(file_name):
+    pattern = r"(s\d{2}e\d{2})"
+    match = re.search(pattern, file_name, re.IGNORECASE)
+    if match:
+        return match.group(1)
+    else:
+        print(f"Error extracting season from {file_name}")
+        return None
+
+def search_episode_name(show_name, file_name):
+    show_name = show_name.replace(" ","+")
+    s00e00 = extract_season(file_name)
+    season_number = s00e00[1:3]
+    episode_number = s00e00[4:6]
+    url=f"http://www.omdbapi.com/?apikey={apiKey}&t={show_name}&season={season_number}&episode={episode_number}"
+    response = requests.get(url)
+    response.raise_for_status()
+    data = response.text
+    data = json.loads(data)
+    episode_name = data['Title']
+    return episode_name
+
+def name_series(file_name, name_combo, info_combo, sub_combo, episode, lowercase):
+    info_delim = info_combo.get()
+    name_delim = name_combo.get()
+    sub_delim = sub_combo.get()
+    
+    show_name = detect_name(file_name, name_delim, sub_delim).replace(".", name_delim).rstrip(name_delim)
+    season = extract_season(file_name)
+    file_ext = get_file_extension(file_name)
+    if episode is not None and episode.get() == 1:
+        episode_name = search_episode_name(show_name, file_name).replace(" ", name_delim)
+        file_name = f"{show_name.replace(".", name_delim).rstrip(name_delim)}{info_delim}{season}{info_delim}{episode_name}{file_ext}"
+    else:
+        file_name = f"{show_name.replace(".", name_delim).rstrip(name_delim)}{info_delim}{season}{file_ext}"
+    if lowercase.get() == 1:
+        return file_name.lower()
+    else:
+        return file_name
+
+# Folder Functions
 
 def parse_folder(folder_path, folder_list):
     folder_list.delete(0, tk.END)
@@ -43,53 +88,38 @@ def parse_folder(folder_path, folder_list):
     for item in contents:
         folder_list.insert(i, item)
 
-def search_episode_name(show_name, file_name):
-    show_name = show_name.replace(" ","+")
-    s00e00 = extract_season(file_name)
-    season_number = s00e00[1:3]
-    episode_number = s00e00[4:6]
-    url=f"http://www.omdbapi.com/?apikey={apiKey}&t={show_name}&season={season_number}&episode={episode_number}"
+# Rest of Functions
 
+def get_file_extension(file_name):
+    _, extension = os.path.splitext(file_name)
+    return extension
 
-    response = requests.get(url)
-    response.raise_for_status()
-
-    data = response.text
-    data = json.loads(data)
-    episode_name = data['Title']
-    return episode_name
-
-# To do: else should search imdb for the year of the movie and pull it if needed
-#        however, this will likely never be an issue since most files usually come
-#        with the year in the file name
-def extract_year(file_name):
-    pattern = r"(\d{4})"
-    match = re.search(pattern, file_name, re.IGNORECASE)
+def update_example(name_combo, info_combo, sub_combo, example_text, media_type, episode, lowercase):
     
-    if match:
-        year = match.group(1)
-        print(year)
-        return year
-    else:
-        print(f"Error extracting year from {file_name}")
-        return None
-    
-def extract_season(file_name):
-    pattern = r"(s\d{2}e\d{2})"
-    match = re.search(pattern, file_name, re.IGNORECASE)
+    name_delim = name_combo.get()
+    info_delim = info_combo.get()
+    sub_delim = sub_combo.get()
+    example_names = ["Movie","Name","Episode","Sub", "Series"]
 
-    if match:
-        season = match.group(1)
-        return season
+    if lowercase.get() == 1:
+        example_names = [name.lower() for name in example_names]
+            
+
+    if media_type == 0:
+        example = f"{example_names[0]}{name_delim}{example_names[1]}{sub_delim}{example_names[3]}{name_delim}{example_names[1]}{info_delim}YEAR.ext"
     else:
-        print(f"Error extracting season from {file_name}")
-        return None
+        if episode is not None and episode.get() == 1:
+            example = f"{example_names[4]}{name_delim}{example_names[1]}{sub_delim}{example_names[3]}{name_delim}{example_names[1]}{info_delim}S00E00{info_delim}{example_names[2]}{name_delim}{example_names[1]}.ext"
+        else:
+            example = f"{example_names[4]}{name_delim}{example_names[1]}{sub_delim}{example_names[3]}{name_delim}{example_names[1]}{info_delim}S00E00.ext"
+    example_text.delete("1.0", tk.END) 
+    example_text.insert(tk.END, example)
 
 def detect_name(file_name, name_delim, sub_title_delim):
     
     pattern = r"(s\d{2}e\d{2})|(\d{4})"
     delim_pattern = r"[._ -]"
-    sub_title_pattern = r":\s?"
+    sub_title_pattern = rf":{name_delim}?"
 
     match = re.search(pattern, file_name, re.IGNORECASE)
 
@@ -116,48 +146,17 @@ def detect_name(file_name, name_delim, sub_title_delim):
 
     print(media_name)
 
-    return media_name
-
-def name_movie(file_name, name_combo, info_combo, sub_combo, lowercase):
-    info_delim = info_combo.get()
-    name_delim = name_combo.get()
-    sub_delim = sub_combo.get()
-    show_name = detect_name(file_name, name_delim, sub_delim)
-    year = extract_year(file_name)
-    file_ext = get_file_extension(file_name)
-    file_name = f"{show_name}{info_delim}{year}{file_ext}"
-    if lowercase.get() == 1:
-        return file_name.lower()
-    else:
-        return file_name
-        
-def name_series(file_name, name_combo, info_combo, episode_option, lowercase):
-    info_delim = info_combo.get()
-    name_delim = name_combo.get()
-    
-    show_name = detect_name(file_name).replace(".", name_delim).rstrip(name_delim)
-    season = extract_season(file_name)
-    file_ext = get_file_extension(file_name)
-    if episode_option.get() == 1:
-        episode_name = search_episode_name(show_name, file_name).replace(" ", name_delim)
-        file_name = f"{show_name.replace(".", name_delim).rstrip(name_delim)}{info_delim}{season}{info_delim}{episode_name}{file_ext}"
-    else:
-        file_name = f"{show_name.replace(".", name_delim).rstrip(name_delim)}{info_delim}{season}{file_ext}"
-    if lowercase.get() == 1:
-        return file_name.lower()
-    else:
-        return file_name
-        
+    return media_name   
     
 def insert_preview(folder_path, preview_list, name_combo, info_combo, sub_combo, media_type, episode_option, lowercase):
     preview_list.delete(0, tk.END)
     contents = os.listdir(folder_path.get())
     contents.sort()
     for item in contents:
-        if media_type.get() == 0:
+        if media_type == 0:
             file_name = name_movie(item, name_combo, info_combo, sub_combo, lowercase)
         else:
-            file_name = name_series(item, name_combo, info_combo, episode_option, lowercase)
+            file_name = name_series(item, name_combo, info_combo, sub_combo, episode_option, lowercase)
         preview_list.insert(1, file_name)
     
 
@@ -166,7 +165,7 @@ def rename_file(folder_entry, name_combo, info_combo, media_type, episode_option
     contents = os.listdir(folder_path)
     contents.sort()
     for item in contents:
-        if media_type.get() == 0:
+        if media_type == 0:
             file_name = name_movie(item, name_combo, info_combo, lowercase)
         else:
             file_name = name_series(item, name_combo, info_combo, episode_option, lowercase)
--- gui.py
+++ gui.py
@@ -19,31 +19,23 @@ series_tab = ttk.Frame(frame)
 notebook.add(series_tab, text="Series")
 
 movie_tab = ttk.Frame(frame)
-notebook.add(movie_tab, text="Movie")
+notebook.add(movie_tab, text="Movies")
 
 widgets = {
     "series": {},
     "movies": {}
 }
 
+def create_combobox(frame, label_text, label_row, label_col, tab_name, widget_name, widget_row, widget_col, values):
+    combobox = tk.Label(frame, text=label_text)
+    combobox.grid(row=label_row, column=label_col)
+    widgets[tab_name][widget_name] = ttk.Combobox(frame, values=values)
+    widgets[tab_name][widget_name].grid(row=widget_row, column=widget_col)
+
 def delim_options(frame, tab_name):
-    name_delim = tk.Label(frame, text="Name Delim")
-    name_delim.grid(row=2, column=0)
-    widgets[tab_name]["name_delim"] = ttk.Combobox(frame, values=['_',' ','-','.'])
-    widgets[tab_name]["name_delim"].grid(row=3, column=0)
-    
-
-    info_delim = tk.Label(frame, text="Info Delim")
-    info_delim.grid(row=2, column=1)
-    widgets[tab_name]["info_delim"] = ttk.Combobox(frame, values=['-',' ','_','.'])
-    widgets[tab_name]["info_delim"].grid(row=3, column=1)
-    
-
-    sub_delim = tk.Label(frame, text="Sub-Title Delim")
-    widgets[tab_name]["sub_delim"] = ttk.Combobox(frame, values=['-',' ','_','.'])
-    sub_delim.grid(row=2, column=2)
-    widgets[tab_name]["sub_delim"].grid(row=3, column=2)
-    
+    create_combobox(frame, "Name Delim", 2, 0, tab_name, "name_delim", 3, 0, ['_',' ','-','.'])
+    create_combobox(frame, "Info Delim", 2, 1, tab_name, "info_delim", 3, 1, ['-',' ','_','.'])
+    create_combobox(frame, "Sub Delim", 2, 2, tab_name, "sub_delim", 3, 2, ['-',' ','_','.'])
 
     widgets[tab_name]["name_delim"].current(0)
     widgets[tab_name]["info_delim"].current(0)
@@ -51,24 +43,52 @@ def delim_options(frame, tab_name):
 
     widgets[tab_name]["lowercase"] = IntVar()
     lowercase_label = tk.Label(frame, text="Case Sensitivity")
+    lowercase_label.grid(row=4, column=0, columnspan=2)
     lowercase_check = tk.Checkbutton(frame, variable=widgets[tab_name]["lowercase"], onvalue=1, offvalue=0, text="Lowercase Names")
-    lowercase_label.grid(row=4, column=0)
-    lowercase_check.grid(row=5, column=0)
+    lowercase_check.grid(row=5, column=0, columnspan=2)
 
 def build_series_tab(frame):
     delim_options(frame, "series")
 
     widgets["series"]["episode"] = IntVar()
     episode_name_label = tk.Label(frame, text="Episode Names")
-    episode_name_label.grid(row=4, column=1)
+    episode_name_label.grid(row=4, column=1, columnspan=2)
     episode_name_check = tk.Checkbutton(frame, variable=widgets["series"]["episode"], onvalue=1, offvalue=0, text="Episode names in file name")
-    episode_name_check.grid(row=5, column=1)
+    episode_name_check.grid(row=5, column=1, columnspan=2)
 
 def build_movie_tab(frame):    
     delim_options(frame, "movies")
 
+def get_selected_tab():
+    return notebook.tab(notebook.select(), "text").lower()
+
+def get_media_type():
+    selected_tab = get_selected_tab()
+    return 1 if selected_tab == "series" else 0
+
 build_movie_tab(movie_tab)
 build_series_tab(series_tab)
+
+def tab_change(event):
+    selected_tab = notebook.tab(notebook.select(), "text").lower()
+    media_type = 1 if selected_tab == "series" else 0
+    update_example(
+        widgets[selected_tab]["name_delim"],
+        widgets[selected_tab]["info_delim"],
+        widgets[selected_tab]["sub_delim"],
+        example_text,
+        media_type,
+        widgets[selected_tab].get("episode", None),  # Handle missing "episode" key gracefully
+        widgets[selected_tab]["lowercase"]
+    )
+
+notebook.bind("<<NotebookTabChanged>>", tab_change)
+
+for widget in series_tab.winfo_children():
+    widget.grid_configure(padx=10, pady=5)
+
+for widget in movie_tab.winfo_children():
+    widget.grid_configure(padx=10, pady=5)
 
 # Example Convention Section
 naming_conv_frame = tk.LabelFrame(frame, text="Naming Convention Example")
@@ -82,21 +102,34 @@ for widget in naming_conv_frame.winfo_children():
 
 # Folder Selection
 show_info_frame = tk.LabelFrame(frame, text="Folder Information")
-show_info_frame.grid(row=2, column=0, columnspan=2, padx=20, pady=20)
+show_info_frame.grid(row=2, column=0, columnspan=2, padx=150, pady=20, sticky='nsew')
 
 folder_name_label = tk.Label(show_info_frame, text="Show Folder")
 folder_name_label.grid(row=0, column=0)
 
-folder_name_entry = tk.Entry(show_info_frame)
-folder_name_entry.grid(row=1, column=0, columnspan=2, sticky='nsew')
+folder_name_entry = tk.Entry(show_info_frame, width=50)
+folder_name_entry.grid(row=1, column=0, columnspan=2)
 
-folder_button = tk.Button(show_info_frame, text="Check Folder", command=lambda: parse_folder(folder_name_entry, folder_list))
+folder_button = tk.Button(show_info_frame, text="Check Folder", command=lambda: parse_folder(folder_name_entry,
+                                                                                             folder_list))
 folder_button.grid(row=1, column=2)
 
-preview_button = tk.Button(show_info_frame, text="Preview", command=lambda: insert_preview(folder_name_entry, preview_list, name_delim_combobox, info_delim_combobox, sub_delim_combobox, mediaType, episode, lowercase))
+preview_button = tk.Button(show_info_frame, text="Preview", command=lambda: insert_preview(folder_name_entry,
+                                                                                           preview_list,
+                                                                                           widgets[get_selected_tab()]["name_delim"],
+                                                                                           widgets[get_selected_tab()]["info_delim"],
+                                                                                           widgets[get_selected_tab()]["sub_delim"],
+                                                                                           get_media_type(),
+                                                                                           widgets[get_selected_tab()].get("episode", None),
+                                                                                           widgets[get_selected_tab()]["lowercase"]))
 preview_button.grid(row=2, column=0, columnspan=2)
 
-rename_button = tk.Button(show_info_frame, text="Rename", command=lambda: rename_file(folder_name_entry, name_delim_combobox, info_delim_combobox, mediaType, episode, lowercase))
+rename_button = tk.Button(show_info_frame, text="Rename", command=lambda: rename_file(folder_name_entry,
+                                                                                      widgets[get_selected_tab()]["name_delim"],
+                                                                                      widgets[get_selected_tab()]["info_delim"],
+                                                                                      get_media_type(),
+                                                                                      widgets[get_selected_tab()].get("episode", None),
+                                                                                      widgets[get_selected_tab()]["lowercase"]))
 rename_button.grid(row=2, column=1, columnspan=2)
 
 for widget in show_info_frame.winfo_children():
@@ -124,19 +157,29 @@ def bind_tab_widgets(tab_name):
 
     if tab_name == "series":
         episode = widgets[tab_name]["episode"]
-        episode.trace_add("write", lambda *args:update_example(name_delim, info_delim, sub_delim, example_text, notebook, episode, lowercase))
-        
+        episode.trace_add("write", lambda *args:update_example(name_delim, info_delim, sub_delim,
+                                                               example_text, get_media_type(),
+                                                               episode, lowercase))
+
     name_delim = widgets[tab_name]["name_delim"]
     info_delim = widgets[tab_name]["info_delim"]
     sub_delim = widgets[tab_name]["sub_delim"]
     lowercase = widgets[tab_name]["lowercase"]
     
 
-    name_delim.bind("<<ComboboxSelected>>", lambda event: update_example(name_delim, info_delim, sub_delim, example_text, notebook, episode, lowercase))
-    info_delim.bind("<<ComboboxSelected>>", lambda event: update_example(name_delim, info_delim, sub_delim, example_text, notebook, episode, lowercase))
-    sub_delim.bind("<<ComboboxSelected>>", lambda event: update_example(name_delim, info_delim, sub_delim, example_text, notebook, episode, lowercase))
+    name_delim.bind("<<ComboboxSelected>>", lambda event: update_example(name_delim, info_delim, sub_delim,
+                                                                         example_text, get_media_type(),
+                                                                         widgets[get_selected_tab()].get("episode", None), lowercase))
+    info_delim.bind("<<ComboboxSelected>>", lambda event: update_example(name_delim, info_delim, sub_delim,
+                                                                         example_text, get_media_type(),
+                                                                         widgets[get_selected_tab()].get("episode", None), lowercase))
+    sub_delim.bind("<<ComboboxSelected>>", lambda event: update_example(name_delim, info_delim, sub_delim,
+                                                                        example_text, get_media_type(),
+                                                                        widgets[get_selected_tab()].get("episode", None), lowercase))
     
-    lowercase.trace_add("write", lambda *args:update_example(name_delim, info_delim, sub_delim, example_text, notebook, episode, lowercase))
+    lowercase.trace_add("write", lambda *args:update_example(name_delim, info_delim, sub_delim,
+                                                             example_text, get_media_type(),
+                                                             widgets[get_selected_tab()].get("episode", None), lowercase))
 
 bind_tab_widgets("series")
 bind_tab_widgets("movies")
@@ -146,8 +189,8 @@ update_example(
     widgets["series"]["info_delim"], 
     widgets["series"]["sub_delim"], 
     example_text, 
-    notebook, 
-    widgets["series"]["episode"], 
+    get_media_type(), 
+    widgets["series"].get("episode", None), 
     widgets["series"]["lowercase"])
 
 
